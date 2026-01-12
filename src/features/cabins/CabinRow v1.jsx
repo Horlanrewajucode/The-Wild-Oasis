7b52e9686202ed8f87ac790125aecca218ee49cf
Horlanrewajucode <buzareeolanrewaju01@gmail.com>
2026-01-12 11:47:05 +0100

    Implemented functionality for a context menu

--- src/features/cabins/CabinRow v1.jsx
+++ src/features/cabins/CabinRow v1.jsx
@@ -88,35 +88,23 @@ function CabinRow({ cabin }) {
           <span>&mdash;</span>
         )}
         <div>
-          {/* <button onClick={handleDuplicate} disabled={isCreating} type="button">
+          <button onClick={handleDuplicate} disabled={isCreating} type="button">
             <HiSquare2Stack />
-          </button> */}
+          </button>
           <Modal>
-            <Menus.Menu>
-              <Menus.Toggle id={cabinId} />
-              <Menus.List id={cabinId}>
-                <Menus.Button
-                  icon={<HiSquare2Stack />}
-                  onClick={handleDuplicate}
-                >
-                  Duplicate
-                </Menus.Button>
-
-                <Modal.Open opens="edit">
-                  <Menus.Button icon={<HiPencil />}>Edit</Menus.Button>
-                </Modal.Open>
-
-                <Modal.Open opens="delete">
-                  <Menus.Button icon={<HiTrash />}>Delete</Menus.Button>
-                </Modal.Open>
-                
-              </Menus.List>
-            </Menus.Menu>
-
+            <Modal.Open opens="edit">
+              <button type="button">
+                <HiPencil />
+              </button>
+            </Modal.Open>
             <Modal.Window name="edit">
               <CreateCabinForm cabinToEdit={cabin} />
             </Modal.Window>
-
+            <Modal.Open opens="delete">
+              <button type="button">
+                <HiTrash />
+              </button>
+            </Modal.Open>
             <Modal.Window name="delete">
               <ConfirmDelete
                 resourceName="cabins"
@@ -125,6 +113,18 @@ function CabinRow({ cabin }) {
               />
             </Modal.Window>
           </Modal>
+          {/* <Menus.Menu>
+            <Menus.Toggle id={cabinId} />
+            <Menus.List id={cabinId}>
+              <Menus.Button icon={<HiSquare2Stack />} onClick={handleDuplicate}>
+                Duplicate
+              </Menus.Button>
+
+              <Menus.Button icon={<HiPencil />}>Edit</Menus.Button>
+
+              <Menus.Button icon={<HiTrash />}>Delete</Menus.Button>
+            </Menus.List>
+          </Menus.Menu> */}
         </div>
       </Table.Row>
     </>
