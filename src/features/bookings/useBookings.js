91ba159034b1afa10c73373caccded65b2f7ef0e
Horlanrewajucode <buzareeolanrewaju01@gmail.com>
2026-01-14 16:03:52 +0100

    Implemented Pagination for bookings page

--- src/features/bookings/useBookings.js
+++ src/features/bookings/useBookings.js
@@ -6,7 +6,6 @@ export function useBookings() {
   const [searchParams] = useSearchParams();
 
   // FILTER
-
   const filterValue = searchParams.get("status");
   const filter =
     !filterValue || filterValue === "all"
@@ -14,19 +13,21 @@ export function useBookings() {
       : { field: "status", value: filterValue };
 
   // SORT
-
   const sortByRaw = searchParams.get("sortBy") || "startDate-desc";
   const [field, direction] = sortByRaw.split("-");
   const sortBy = { field, direction };
 
+  // PAGINATION
+  const page = !searchParams.get("page") ? 1 : Number(searchParams.get("page"));
+
   const {
     isPending,
-    data: bookings,
+    data: { data: bookings, count } = {},
     error,
   } = useQuery({
-    queryKey: ["bookings", filter, sortBy],
-    queryFn: () => getBookings({ filter, sortBy }),
+    queryKey: ["bookings", filter, sortBy, page],
+    queryFn: () => getBookings({ filter, sortBy, page }),
   });
 
-  return { isPending, error, bookings };
+  return { isPending, error, bookings, count };
 }
