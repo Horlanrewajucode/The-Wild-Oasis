a15abef7615cd9c9748c20312011352fae74cf82
Horlanrewajucode <buzareeolanrewaju01@gmail.com>
2026-01-20 10:01:23 +0100

    Implemented functionality for checking out a booking

--- src/features/bookings/BookingRow.jsx
+++ src/features/bookings/BookingRow.jsx
@@ -6,9 +6,10 @@ import Table from "../../ui/Table";
 
 import { formatCurrency } from "../../utils/helpers";
 import { formatDistanceFromNow } from "../../utils/helpers";
-import { HiArrowDownOnSquare, HiEye } from "react-icons/hi2";
+import { HiArrowDownOnSquare, HiArrowUpOnSquare, HiEye } from "react-icons/hi2";
 import Menus from "../../ui/Menus";
 import { useNavigate } from "react-router-dom";
+import { useCheckout } from "../check-in-out/useCheckout";
 
 const Cabin = styled.div`
   font-size: 1.6rem;
@@ -52,6 +53,7 @@ function BookingRow({
   },
 }) {
   const navigate = useNavigate();
+  const { checkout, isCheckingOut } = useCheckout();
 
   const statusToTagName = {
     unconfirmed: "blue",
@@ -88,14 +90,13 @@ function BookingRow({
       <Menus.Menu>
         <Menus.Toggle id={bookingId} />
         <Menus.List id={bookingId}>
-          
           <Menus.Button
             icon={<HiEye />}
             onClick={() => navigate(`/bookings/${bookingId}`)}
           >
             See Details
           </Menus.Button>
-          
+
           {status === "unconfirmed" && (
             <Menus.Button
               icon={<HiArrowDownOnSquare />}
@@ -104,6 +105,17 @@ function BookingRow({
               Check In
             </Menus.Button>
           )}
+
+          {status === "checked-in" && (
+            <Menus.Button
+              icon={<HiArrowUpOnSquare />}
+              onClick={() => checkout(bookingId)}
+              disabled={isCheckingOut}
+            >
+              Check Out
+            </Menus.Button>
+          )}
+          
         </Menus.List>
       </Menus.Menu>
     </Table.Row>
--- src/features/check-in-out/useCheckout.js
+++ src/features/check-in-out/useCheckout.js
@@ -1,27 +1,22 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { updateBooking } from "../../services/apiBookings";
 import toast from "react-hot-toast";
-import { useNavigate } from "react-router-dom";
 
-export function useCheckin() {
+export function useCheckout() {
   const queryClient = useQueryClient();
-  const navigate = useNavigate();
 
-  const { mutate: checkin, isPending: isCheckingIn } = useMutation({
-    mutationFn: ({ bookingId, breakfast }) =>
+  const { mutate: checkout, isPending: isCheckingOut } = useMutation({
+    mutationFn: (bookingId) =>
       updateBooking(bookingId, {
-        status: "checked-in",
-        isPaid: true,
-        ...breakfast,
+        status: "checked-out",
       }),
 
     onSuccess: (data) => {
-      toast.success(`Booking #${data.id} Successfully Checked In`);
+      toast.success(`Booking #${data.id} Successfully Checked Out`);
       queryClient.invalidateQueries({ active: true });
-      navigate("/");
     },
 
     onError: () => toast.error("There was an error while checking Out"),
   });
-  return { checkin, isCheckingIn };
+  return { checkout, isCheckingOut };
 }
