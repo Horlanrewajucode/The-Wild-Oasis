d2921c894a0ff83ddf6318ab2fcecd1b5b14cf41
Horlanrewajucode <buzareeolanrewaju01@gmail.com>
2026-01-24 17:19:26 +0100

    Built Statistics layout for dashboard

--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -1,4 +1,9 @@
 import styled from "styled-components";
+import { useRecentBooking } from "./useRecentBooking";
+import Spinner from "../../ui/Spinner";
+import { useRecentStays } from "./useRecentStays";
+import Stats from "./stats";
+import { useCabins } from "../cabins/useCabins";
 
 const StyledDashboardLayout = styled.div`
   display: grid;
@@ -6,3 +11,32 @@ const StyledDashboardLayout = styled.div`
   grid-template-rows: auto 34rem auto;
   gap: 2.4rem;
 `;
+
+function DashboardLayout() {
+  const { bookings, isPending: isPendingBooking } = useRecentBooking();
+  const {
+    stays,
+    isPending: isPendingStays,
+    confirmedStays,
+    numDays,
+  } = useRecentStays();
+  const { cabins, isPending: isPendingCabins } = useCabins();
+
+  if (isPendingBooking || isPendingStays || isPendingCabins) return <Spinner />;
+
+  return (
+    <StyledDashboardLayout>
+      <Stats
+        bookings={bookings}
+        confirmedStays={confirmedStays}
+        numDays={numDays}
+        cabinCount={cabins.length}
+      />
+      <div>Today's Activities</div>
+      <div>Chart Stay Duration</div>
+      <div>Chart Sales</div>
+    </StyledDashboardLayout>
+  );
+}
+
+export default DashboardLayout;
--- src/services/apiBookings.js
+++ src/services/apiBookings.js
@@ -53,10 +53,11 @@ export async function getBooking(id) {
 }
 
 // Returns all BOOKINGS that were created after the given date. Useful to get bookings created in the last 30 days, for example.
+// date: ISOString
 export async function getBookingsAfterDate(date) {
   const { data, error } = await supabase
     .from("bookings")
-    .select("created_at, totalPrice, extrasPrice")
+    .select("created_at, totalPrice, extraPrice")
     .gte("created_at", date)
     .lte("created_at", getToday({ end: true }));
 
@@ -72,7 +73,6 @@ export async function getBookingsAfterDate(date) {
 export async function getStaysAfterDate(date) {
   const { data, error } = await supabase
     .from("bookings")
-    // .select('*')
     .select("*, guests(fullName)")
     .gte("startDate", date)
     .lte("startDate", getToday());
